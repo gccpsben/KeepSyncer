70e863ea45c9d81ef31e3f66573db36e7ddabf1d
gccpsben <gccpsben@gmail.com>
2024-01-18 17:50:44 +0800

    Stop tracking server_build

--- server/server_build/settings.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.Settings = void 0;
-var Settings;
-(function (Settings) {
-    Settings.AuthTokenAge = 2.592e+9; // 30 days
-    Settings.QRCodeTokenAge = 60000;
-})(Settings = exports.Settings || (exports.Settings = {}));
